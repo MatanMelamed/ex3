c5d3424d624046e2b047f3d5fdbd39ec940da615
Matan <melamed.matan1@gmail.com>
2019-05-29 22:07:38 +0300

    Add project files.

---
+++ ex3/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ex3
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Displayer", action = "Display" }
+            );
+
+            routes.MapRoute(
+                name: "Displayer",
+                url: "display/{first}/{second}/{rate}",
+                defaults: new { controller = "Displayer", action = "Display", rate = UrlParameter.Optional }
+            );
+
+            //routes.MapRoute(
+            //    name: "DisplayContinuous",
+            //    url: "display/{first}/{second}/{rate}",
+            //    defaults: new { controller = "Displayer", action = "DisplayContinuous" }
+            //);
+
+            routes.MapRoute(
+                name: "Saver",
+                url: "save/{ip}/{port}/{rate}/{time}/{name}",
+                defaults: new { controller = "Save", action = "save" }
+            );
+        }
+    }
+}
---
+++ ex3/Models/GeneratorForTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ex3.Models {
+    public class GeneratorForTests {
+
+        int counter;
+
+        private static GeneratorForTests instane = null;
+        public static GeneratorForTests Instance {
+            get {
+                if (instane == null) {
+                    instane = new GeneratorForTests();
+                    instane.counter = 1;
+                }
+
+                return instane;
+            }
+        }
+
+        public double[] newPoint {
+            get {
+                double[] point = new double[2];
+                point[0] = 180 - counter * 2 ;
+                point[1] = -90 + counter * 1;
+                counter++;
+                return point;
+            }
+        }
+    }
+}
